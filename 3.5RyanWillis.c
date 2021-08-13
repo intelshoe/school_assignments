93cd34bb6fac9e2d0b8a31d363930613a0f386d0
Just A Coder <61443129+intelshoe@users.noreply.github.com>
2021-08-12 21:26:13 -0400

    Add files via upload

---
+++ 3.5RyanWillis.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void do_fun(int *amtAddres);    // function which modifies value of amt
+
+int main()
+{
+    int amt;
+    
+    amt = 100;  // Initially set to 100
+    printf("In main(), amt is %d \n", amt);
+    
+    do_fun(&amt);      // Pass the memory address of amt
+    printf("After return, amt is %d in main() \n", amt);
+    return 0;
+    
+    //wait for keypress to exit
+    printf("\n\nPress any key to exit.");
+    getchar();
+}
+
+void do_fun(int *amtAddress)  // Gets passed the memory location for variable amt
+{
+     *amtAddress = 85;        // The value in memory location for variable amt
+                              // is set to 85
+     printf("In do_fun(), amt is %d \n", *amtAddress);
+     return;
+}
