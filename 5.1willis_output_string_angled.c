021168430c067cadd5d4a282170fa3e1ff26b706
Just A Coder <61443129+intelshoe@users.noreply.github.com>
2021-08-26 20:13:50 -0400

    Update 5.1willis_output_string_angled.c

--- 5.1willis_output_string_angled.c
+++ 5.1willis_output_string_angled.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+#include <string.h>     // for strcpy_s, strcat_s
 
 int main(void)
 {
     puts("String output using character array");
     char h[] = "Happy Birthday"; //initialize char array h
+    char angle[500] = "\t";
     for (size_t i = 0; h[i] != '\0'; ++i)
     {
-        printf("\t%c \n", h[i]);
+        strcat_s(angle, sizeof angle, "\t");
+
+        printf("%s %c \n", angle, h[i]);
     }
     printf("\n\n");
-    
+
     //wait for keypress to exit
     printf("\n\nPress any key to exit.");
     getchar();
