30210a9d2b5cd19c85fe65caf471584c4c8169a9
Just A Coder <61443129+intelshoe@users.noreply.github.com>
2021-08-28 16:12:30 -0400

    Update mathpractice_finalproject.c

--- mathpractice_finalproject.c
+++ mathpractice_finalproject.c
@@ -3,6 +3,7 @@
 //5.3 Math Practice Week 5 (Final)
 
 #include <stdio.h>
+#include <stdbool.h> //for the bool type
 
 //function prototypes
 void addition(int d, int *score); // pass d for difficulty level and *score for # correct
@@ -10,6 +11,7 @@ void subtract(int d, int *score);
 void multiply(int d, int *score);
 void divide(int d, int *score);
 int difficulty(char level);
+void attempt(bool rightOrWrong, int *score);
 
 //function main begins program
 int main()
@@ -17,7 +19,7 @@ int main()
     int option = 0;               //menu options
     char level;  //takes character difficulty input
     int d; //level digit for rand generation
-    int score[2]; // element 0 stores wrong answer amt, 1 stores correct answer amount
+    int score[2] = {0}; // element 0 stores wrong answer amt, 1 stores correct answer amount
     
     //go back to menu as long as option 5 (quit) not selected
     while (option != 5) {
@@ -30,8 +32,10 @@ int main()
         printf("\n5. Exit");
         printf("\nSelect an option: ");
         scanf("%d", &option);
+        
         //breaks out of while loop if option 5 chosen
         if (option == 5) { break; }
+        
         printf("\nPlease enter difficulty level e-easy, m-medium, or h-hard. ");
         label:
         scanf(" %c", &level);
@@ -40,22 +44,24 @@ int main()
         if (d == 1400) {
            goto label;    // jump back up
            }
+           
         //code for addition
         else if (option == 1) {
-            addition(d, *score); // pass d for difficulty level and *score for # correct
+            addition(d, score); // pass d for difficulty level and *score for # correct
         }
         else if (option == 2) {
             //calls subtraction function
-            subtract(d, *score);
+            subtract(d, score);
         }
         else if (option == 3) {
             //calls multiply function
-            multiply(d, *score);
+            multiply(d, score);
         }
         else if (option == 4) {
             //calls divide function
-            divide(d, *score);
+            divide(d, score);
         }
+        printf("Current total correct answers: %d, total wrong answers: %d\n\n", score[1], score[0]); 
     }
 
 }
@@ -76,27 +82,51 @@ int difficulty(char level) {
     }
 }
 
+//what to do if correct answer or incorrect answer
+void attempt(bool rightOrWrong, int *score){
+     if (rightOrWrong == true){
+       printf("\nCorrect!\n\n");
+       score[1] += 1; // increment correct score-count
+    }
+    else {
+         printf("\nSorry, play again.\n");
+         score[0] += 1; // increment incorrect score-count
+    }
+
+}
 
 
-void addition(int d) {
+
+void addition(int d, int *score) {
     int guess = 0;
     int num1;
     int num2;
+    int count = 1; // keeps track of attempts
 
+    //calculate problem based on d(difficulty level)
     num1 = rand() % d;
     num2 = rand() % d;
     printf("\n%d + %d = ? ", num1, num2);
+
+do{
+    //user input given:
+    printf("\nTry #%d: ", count);
     scanf("%d", &guess);
     
+    //decide if right or wrong
     if (guess == (num1 + num2)) {
-       printf("\nCorrect!\n\n");
+         attempt(true, score);
+         break; // no need to loop if correct answer
     }
     else {
-       printf("\nSorry, play again.\n\n");
+         attempt(false, score);
+         count++;
     }
+    }while (count < 4);
+            
 }
 
-void subtract(int d) {
+void subtract(int d, int *score) {
      int guess = 0;
      int num1 = rand() % d;
      int num2 = rand() % d;
@@ -112,7 +142,7 @@ void subtract(int d) {
      }
 }
 
-void multiply(int d) {
+void multiply(int d, int *score) {
      int guess = 0;
      int num1 = rand() % d;
      int num2 = rand() % d;
@@ -128,7 +158,7 @@ void multiply(int d) {
      }
 }
 
-void divide(int d) {
+void divide(int d, int *score) {
      int guess = 0;
      int num1 = rand() % d + 10;
      int num2 = rand() % d + 5;
